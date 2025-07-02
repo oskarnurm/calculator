c383c27fe03e45044038cfc695ae9d9068bee468
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-07-02 16:16:30 +0300

    chore: add buttons with content and event listeners
    Added buttons for numbers and operators with event listeners using some
    DOM manipulations with js

--- index.js
+++ index.js
@@ -15,3 +15,30 @@ function operate(operator, a, b) {
   if (operator === "/") return divide(a, b);
   throw new Error(`Unknown operator ${operator}`);
 }
+
+const numContainer = document.querySelector(".numbers");
+const opsContainer = document.querySelector(".operators");
+
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", "="];
+const operators = ["/", "*", "-", "+"];
+
+numbers.forEach((number) => {
+  const btn = document.createElement("button");
+  btn.textContent = number;
+  btn.classList.add("number");
+  numContainer.appendChild(btn);
+});
+
+operators.forEach((operator) => {
+  const btn = document.createElement("button");
+  btn.textContent = operator;
+  btn.classList.add("operator");
+  opsContainer.appendChild(btn);
+});
+
+const calculator = document.querySelector(".calculator");
+calculator.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON") {
+    console.log(e.target.textContent);
+  }
+});

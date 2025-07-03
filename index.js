6bc74b96717ac7ad9e656406210188fd304ca56e
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-07-03 15:48:49 +0300

    feat: add evaluation logic and eror handeling
    The calculator now wont evaluate more than a single pair of numbers at a
    time. Long decimal answers are rounded. Added a justEvaluated check so
    pressing "=" does not cause problems when there is nothing to evaluate
    and the display is cleared when pressing a new number after evaluation.
    We display a snarky error message when trying to divide by zero and we
    don't allow more than one decimal in a number, still need to add
    feedback about that.

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-let storage = [];
-const clear = () => (storage = []);
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const mutiply = (a, b) => a * b;
-function divide(a, b) {
+let currentNum = "";
+let tokens = [];
+
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", "="];
+const operators = ["/", "*", "-", "+"];
+
+const clear = () => (display = []);
+const add = (a, b = 0) => +(a + b).toFixed(4);
+const subtract = (a, b = 0) => +(a - b).toFixed(4);
+const mutiply = (a, b = 1) => +(a * b).toFixed(4);
+function divide(a, b = 1) {
   if (b === 0) {
-    throw new Error("Lets not break math, OK?");
+    return "Lets not break math, OK?";
   }
-  return a / b;
+  return +(a / b).toFixed(4);
 }
 
-function operate(operator, a, b) {
+function operate(a, operator, b) {
   if (operator === "+") return add(a, b);
   if (operator === "-") return subtract(a, b);
   if (operator === "*") return mutiply(a, b);
   if (operator === "/") return divide(a, b);
   throw new Error(`Unknown operator ${operator}`);
 }
-
-const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", "="];
-const operators = ["/", "*", "-", "+"];
 
 const numContainer = document.querySelector(".numbers");
 numbers.forEach((number) => {
@@ -37,23 +39,75 @@ operators.forEach((operator) => {
   opsContainer.appendChild(btn);
 });
 
-function updateDisplay() {
-  const display = document.querySelector(".display");
-  display.textContent = storage.join("");
-}
-
+const displayContainer = document.querySelector(".display");
 const calculatorContainer = document.querySelector(".calculator");
+
+let justEvaluated = false;
+
 calculatorContainer.addEventListener("click", (e) => {
   if (e.target.tagName === "BUTTON") {
-    buttonContent = e.target.textContent;
-    if (buttonContent === "C") {
-      storage = [];
-      updateDisplay();
-    } else if (buttonContent === "=") {
-      console.log("operate");
-    } else {
-      storage.push(buttonContent);
-      updateDisplay();
+    input = e.target.textContent;
+    if (justEvaluated) {
+      tokens = [];
+      currentNum = "";
+      displayContainer.textContent = "";
+      justEvaluated = false;
+    }
+
+    // push the last input and calculate the result
+    if (input === "=") {
+      if (currentNum.length > 0) {
+        tokens.push(parseFloat(currentNum));
+      }
+      if (tokens.length >= 2) {
+        console.log(tokens, tokens.length);
+        displayContainer.textContent = operate(tokens[0], tokens[1], tokens[2]);
+      }
+      justEvaluated = true;
+    }
+
+    // parse numbers
+    if (numbers.includes(input) && input !== "=") {
+      // ignore duplicate dot
+      if (input === "." && currentNum.includes(".")) {
+        return;
+      }
+      currentNum += input;
+      displayContainer.textContent += input;
+    }
+
+    // Push or update the operator. Also push the number if we were parsing one.
+    // We also calculate the result of the first two numbers if we can.
+    if (operators.includes(input)) {
+      if (currentNum.length > 0) {
+        const number = parseFloat(currentNum);
+        Number.isNaN(number)
+          ? (displayContainer.textContent = "Invalid number")
+          : tokens.push(number);
+        tokens.push(input);
+        if (tokens.length >= 3) {
+          const result = operate(tokens[0], tokens[1], tokens[2]);
+          tokens.splice(0, 3, result);
+          // can remove this print?
+          // displayContainer.textContent = tokens.join("");
+        }
+        currentNum = "";
+        displayContainer.textContent = tokens.join("");
+      } else if (tokens.length > 0) {
+        // update the operator
+        tokens[1] = input;
+        displayContainer.textContent = tokens.join("");
+      } else {
+        // Do nothing
+        console.log("Number must precede operator");
+      }
+    }
+
+    // clear the display and storage
+    if (input === "C") {
+      currentNum = "";
+      tokens = [];
+      displayContainer.textContent = "";
     }
   }
 });

b7e26bdc141c201b7e9ac9069bb7943267259609
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-07-03 20:26:21 +0300

    chore: add a few extra guard cases and remove redundancy

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 let currentNum = "";
-let tokens = [];
+let storage = [];
 
-const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", "="];
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
 const operators = ["/", "*", "-", "+"];
 
-const clear = () => (display = []);
 const add = (a, b = 0) => +(a + b).toFixed(4);
 const subtract = (a, b = 0) => +(a - b).toFixed(4);
 const mutiply = (a, b = 1) => +(a * b).toFixed(4);
@@ -15,82 +14,83 @@ function divide(a, b = 1) {
   return +(a / b).toFixed(4);
 }
 
-function operate(a, operator, b) {
+function calculate(a, operator, b) {
   if (operator === "+") return add(a, b);
   if (operator === "-") return subtract(a, b);
   if (operator === "*") return mutiply(a, b);
   if (operator === "/") return divide(a, b);
-  throw new Error(`Unknown operator ${operator}`);
+  throw new Error(`Unknown operator: ${operator}`);
 }
 
-const displayContainer = document.querySelector(".display");
-const calculatorContainer = document.querySelector(".calculator");
+const display = document.querySelector(".display");
+const calculator = document.querySelector(".calculator");
 const dotBtn = document.getElementById("dot");
 
 let justEvaluated = false;
 
-calculatorContainer.addEventListener("click", (e) => {
+calculator.addEventListener("click", (e) => {
   if (e.target.tagName === "BUTTON") {
     input = e.target.textContent;
     if (justEvaluated) {
-      tokens = [];
+      storage = [];
       currentNum = "";
-      displayContainer.textContent = "";
+      display.textContent = "";
       dotBtn.disabled = false;
       justEvaluated = false;
     }
 
-    // push the last input and calculate the result
     if (input === "=") {
-      if (currentNum.length > 0) {
-        tokens.push(parseFloat(currentNum));
+      if (currentNum.length) {
+        storage.push(parseFloat(currentNum));
+        currentNum = "";
       }
-      if (tokens.length >= 2) {
-        displayContainer.textContent = operate(tokens[0], tokens[1], tokens[2]);
-      }
+      // We exit early to quard against a case when we try to calculate without
+      // enough input
+      if (storage.length < 3) return;
+      display.textContent = calculate(storage[0], storage[1], storage[2]);
       dotBtn.disabled = false;
       justEvaluated = true;
     }
 
-    // parse numbers
-    if (numbers.includes(input) && input !== "=") {
-      // ignore duplicate dot
-      if (input === "." || currentNum.includes(".")) {
+    if (input === ".") {
+      if (!currentNum.includes(".")) {
+        currentNum += input;
+        display.textContent += input;
+        // Disable the "." button after the number buffer already includes one
+        // to prevent the user from entering an illegal number
         dotBtn.disabled = true;
       }
+    } else if (numbers.includes(input)) {
       currentNum += input;
-      displayContainer.textContent += input;
+      display.textContent += input;
     }
 
     // Push or update the operator. Also push the number if we were parsing one.
     // We also calculate the result of the first two numbers if we can.
     if (operators.includes(input)) {
-      if (currentNum.length > 0) {
-        const number = parseFloat(currentNum);
-        tokens.push(number);
-        tokens.push(input);
+      if (currentNum.length) {
+        storage.push(parseFloat(currentNum));
+        storage.push(input);
         dotBtn.disabled = false;
-        if (tokens.length >= 3) {
-          const result = operate(tokens[0], tokens[1], tokens[2]);
-          tokens.splice(0, 3, result);
+
+        if (storage.length >= 3) {
+          const result = calculate(storage[0], storage[1], storage[2]);
+          storage.splice(0, 3, result);
         }
         currentNum = "";
-        displayContainer.textContent = tokens.join("");
-      } else if (tokens.length > 0) {
-        // update the operator
-        tokens[1] = input;
-        displayContainer.textContent = tokens.join("");
-      } else {
-        // Do nothing
-        console.log("Number must precede operator");
+        display.textContent = storage.join("");
+      } else if (storage.length > 0) {
+        // Update the operator
+        storage[1] = input;
+        display.textContent = storage.join("");
       }
     }
 
-    // clear the display and storage
+    // Clear the display and storage
     if (input === "C") {
       currentNum = "";
-      tokens = [];
-      displayContainer.textContent = "";
+      storage = [];
+      display.textContent = "";
       dotBtn.disabled = false;
     }
   }

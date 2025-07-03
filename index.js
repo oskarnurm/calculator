fea6cc40b39ab2af096107c71df886c90947dc62
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-07-03 12:18:15 +0300

    feat: add live display and clear functionality
    We want the user to get immediate feedback for button presses so we push
    each keypress into a storage variable and render it. The user can also
    press "C" that clears the storage and display.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let storage = [];
+const clear = () => (storage = []);
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const mutiply = (a, b) => a * b;
@@ -16,12 +18,10 @@ function operate(operator, a, b) {
   throw new Error(`Unknown operator ${operator}`);
 }
 
-const numContainer = document.querySelector(".numbers");
-const opsContainer = document.querySelector(".operators");
-
 const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", "="];
 const operators = ["/", "*", "-", "+"];
 
+const numContainer = document.querySelector(".numbers");
 numbers.forEach((number) => {
   const btn = document.createElement("button");
   btn.textContent = number;
@@ -29,6 +29,7 @@ numbers.forEach((number) => {
   numContainer.appendChild(btn);
 });
 
+const opsContainer = document.querySelector(".operators");
 operators.forEach((operator) => {
   const btn = document.createElement("button");
   btn.textContent = operator;
@@ -36,9 +37,23 @@ operators.forEach((operator) => {
   opsContainer.appendChild(btn);
 });
 
-const calculator = document.querySelector(".calculator");
-calculator.addEventListener("click", (e) => {
+function updateDisplay() {
+  const display = document.querySelector(".display");
+  display.textContent = storage.join("");
+}
+
+const calculatorContainer = document.querySelector(".calculator");
+calculatorContainer.addEventListener("click", (e) => {
   if (e.target.tagName === "BUTTON") {
-    console.log(e.target.textContent);
+    buttonContent = e.target.textContent;
+    if (buttonContent === "C") {
+      storage = [];
+      updateDisplay();
+    } else if (buttonContent === "=") {
+      console.log("operate");
+    } else {
+      storage.push(buttonContent);
+      updateDisplay();
+    }
   }
 });

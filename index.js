af2fb4367c39c016b45ba4cb2969ad744bd9101f
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-07-04 23:40:55 +0300

    feat: add backspace and precent operator and refactor
    I did a massive overhaul of the code, hopefully making it more readable

--- index.js
+++ index.js
@@ -1,97 +1,140 @@
-let currentNum = "";
-let storage = [];
-
-const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
-const operators = ["÷", "×", "-", "+"];
-
-const add = (a, b = 0) => +(a + b).toFixed(4);
-const subtract = (a, b = 0) => +(a - b).toFixed(4);
-const mutiply = (a, b = 1) => +(a * b).toFixed(4);
-function divide(a, b = 1) {
-  if (b === 0) {
-    return "Undefined";
-  }
-  return +(a / b).toFixed(4);
-}
-
-function calculate(a, operator, b) {
-  if (operator === "+") return add(a, b);
-  if (operator === "-") return subtract(a, b);
-  if (operator === "×") return mutiply(a, b);
-  if (operator === "÷") return divide(a, b);
-  throw new Error(`Unknown operator: ${operator}`);
-}
-
-const display = document.querySelector(".display");
+const display = document.querySelector(".display__content");
 const calculator = document.querySelector(".calculator");
 const dotBtn = document.querySelector(".dot");
 
 let justEvaluated = false;
+let inputBuffer = "";
+let inputStack = [];
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
+const operators = ["÷", "×", "-", "+"];
+
+const mapOperatorToFunction = {
+  "+": (a, b) => a + b,
+  "-": (a, b) => a - b,
+  "×": (a, b) => a * b,
+  "÷": (a, b) => (b === 0 ? "Undefined" : a / b),
+};
+
+function calculate(a, op, b) {
+  a = parseFloat(a);
+  b = parseFloat(b);
+  const fn = mapOperatorToFunction[op];
+  return +fn(a, b).toFixed(4);
+}
 
 calculator.addEventListener("click", (e) => {
   if (e.target.tagName === "BUTTON") {
-    input = e.target.textContent;
-    if (justEvaluated) {
-      storage = [];
-      currentNum = "";
-      display.textContent = "";
-      dotBtn.disabled = false;
-      justEvaluated = false;
-    }
-
-    if (input === "=") {
-      if (currentNum.length) {
-        storage.push(parseFloat(currentNum));
-        currentNum = "";
-      }
-      // We exit early to quard against a case when we try to calculate without
-      // enough input
-      if (storage.length < 3) return;
-      display.textContent = calculate(storage[0], storage[1], storage[2]);
-      dotBtn.disabled = false;
-      justEvaluated = true;
-    }
-
-    if (input === ".") {
-      if (!currentNum.includes(".")) {
-        currentNum += input;
-        display.textContent += input;
-        // Disable the "." button after the number buffer already includes one
-        // to prevent the user from entering an illegal number
-        dotBtn.disabled = true;
-      }
-    } else if (numbers.includes(input)) {
-      currentNum += input;
-      display.textContent += input;
-    }
-
-    // Push or update the operator. Also push the number if we were parsing one.
-    // We also calculate the result of the first two numbers if we can.
-    if (operators.includes(input)) {
-      if (currentNum.length) {
-        storage.push(parseFloat(currentNum));
-        storage.push(input);
-        dotBtn.disabled = false;
-
-        if (storage.length >= 3) {
-          const result = calculate(storage[0], storage[1], storage[2]);
-          storage.splice(0, 3, result);
-        }
-        currentNum = "";
-        display.textContent = storage.join("");
-      } else if (storage.length > 0) {
-        // Update the operator
-        storage[1] = input;
-        display.textContent = storage.join("");
-      }
-    }
-
-    // Clear the display and storage
-    if (input === "CE") {
-      currentNum = "";
-      storage = [];
-      display.textContent = "";
-      dotBtn.disabled = false;
-    }
+    handleInput(e.target.textContent);
   }
 });
+
+function handleInput(input) {
+  if (justEvaluated) {
+    resetCalculator();
+  }
+  if (input === "=") {
+    handleEquals();
+  }
+  if (input === "%") {
+    handlePrecent();
+  }
+  if (numbers.includes(input)) {
+    handleNumbers(input);
+  }
+  if (operators.includes(input)) {
+    handleOperators(input);
+  }
+  if (input === "AC") {
+    resetCalculator();
+  }
+  if (input === "C") {
+    handleBackspace();
+  }
+}
+
+function resetCalculator() {
+  inputStack = [];
+  inputBuffer = "";
+  updateDisplay("");
+  dotBtn.disabled = false;
+  justEvaluated = false;
+}
+
+function updateDisplay(input) {
+  display.textContent = input;
+}
+
+function handleEquals() {
+  // Ignore repeated input and when we can't calculate
+  if (justEvaluated || inputStack < 3) {
+    return;
+  }
+  if (inputBuffer) {
+    inputStack.push(inputBuffer);
+  }
+  const result = calculate(inputStack[0], inputStack[1], inputStack[2]);
+  updateDisplay(result);
+  currentNum = "";
+  dotBtn.disabled = false;
+  justEvaluated = true;
+}
+
+function handlePrecent() {
+  if (!inputBuffer) return;
+  const precent = (parseFloat(inputBuffer) / 100).toString();
+  updateDisplay(precent);
+  justEvaluated = true;
+}
+
+function handleNumbers(number) {
+  // Prevent multple decimal points
+  if (number === "." && inputBuffer.includes(".")) return;
+  inputBuffer += number;
+  display.textContent += number;
+  dotBtn.disabled = inputBuffer.includes(".");
+}
+
+function handleOperators(operator) {
+  if (inputBuffer) {
+    inputStack.push(inputBuffer);
+    inputBuffer = "";
+    inputStack.push(operator);
+    dotBtn.disabled = false;
+
+    // Get intermediate result
+    if (inputStack.length >= 3) {
+      const result = calculate(inputStack[0], inputStack[1], inputStack[2]);
+      inputStack.splice(0, 3, result);
+      updateDisplay(inputStack.join(""));
+    }
+  }
+  // Update operator in the stack (the last input) when inputBuffer is empty (if applicable)
+  if (inputStack.length > 0) {
+    inputStack[inputStack.length - 1] = operator;
+    updateDisplay(inputStack.join(""));
+  }
+}
+
+function handleBackspace() {
+  if (inputBuffer) {
+    inputBuffer = inputBuffer.slice(0, -1);
+    updateDisplay(inputStack.join("") + inputBuffer);
+    // Because we might delete the dot
+    dotBtn.disabled = inputBuffer.includes(".");
+  } else if (inputStack.length) {
+    const lastInput = inputStack[inputStack.length - 1];
+    // If the input we are about to remove is an operator we can simply pop
+    // Otherwise we have to delete character by character if the input is a number
+    if (operators.includes(lastInput)) {
+      inputStack.pop();
+    } else {
+      let numStr = lastInput.toString();
+      numStr = numStr.slice(0, -1);
+      if (numStr) {
+        inputStack[inputStack.length - 1] = numStr;
+      } else inputStack.pop();
+    }
+    updateDisplay(inputStack.join(""));
+    dotBtn.disabled = false;
+  }
+}
